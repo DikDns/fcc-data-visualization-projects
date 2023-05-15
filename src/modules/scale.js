07445d535aa923d65b60942111cee8ee9d971ee5
DikDns <dikdnssocial@gmail.com>
2023-05-15 19:25:35 +0700

    add: scales

--- src/modules/scale.js
+++ src/modules/scale.js
@@ -1,1 +1,16 @@
 import { scaleLinear, scaleTime } from "d3";
+
+function timeScale(minDomain, maxDomain, minRange, maxRange) {
+  const newMaxDomain = new Date(maxDomain.getFullYear() + 1, 1, 1);
+  return scaleTime()
+    .domain([minDomain, newMaxDomain])
+    .range([minRange, maxRange]);
+}
+
+function linearScale(minDomain, maxDomain, minRange, maxRange) {
+  return scaleLinear()
+    .domain([minDomain, maxDomain])
+    .range([minRange, maxRange]);
+}
+
+export { timeScale, linearScale };

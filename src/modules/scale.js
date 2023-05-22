84e9ed0036cb8b9be10e97eb4a33002a7191a5b3
DikDns <dikdnssocial@gmail.com>
2023-05-22 08:30:42 +0700

    add: thresholdScale

--- src/modules/scale.js
+++ src/modules/scale.js
@@ -1,4 +1,4 @@
-import { scaleLinear, scaleTime } from "d3";
+import { scaleLinear, scaleTime, scaleThreshold } from "d3";
 
 function timeScale(minDomain, maxDomain, minRange, maxRange) {
   return scaleTime().domain([minDomain, maxDomain]).range([minRange, maxRange]);
@@ -10,4 +10,16 @@ function linearScale(minDomain, maxDomain, minRange, maxRange) {
     .range([minRange, maxRange]);
 }
 
-export { timeScale, linearScale };
+function thresholdScale(minDomain, maxDomain, inputRange) {
+  const domainArr = [];
+  const count = inputRange.length;
+  const step = (minDomain - maxDomain) / count;
+
+  for (let i = 1; i < count; i++) {
+    domainArr.push(minDomain + i * step);
+  }
+
+  return scaleThreshold().domain(domainArr).range(inputRange);
+}
+
+export { timeScale, linearScale, thresholdScale };

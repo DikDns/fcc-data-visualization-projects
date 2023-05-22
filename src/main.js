f4ab362834b7461ebf471bf7ca761bebfc092819
DikDns <dikdnssocial@gmail.com>
2023-05-22 09:39:40 +0700

    add: legend svg

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import "./style.css";
 import { min, max, select } from "d3";
 import { getGlobalTemperature } from "./modules/data";
-import { linearScale, timeScale } from "./modules/scale";
+import { linearScale, thresholdScale } from "./modules/scale";
 import { addSvg } from "./components/svg";
 import { addText } from "./components/text";
 import { addAxis, createAxis, timeFormat } from "./components/axis";
@@ -82,6 +82,69 @@ async function main() {
   addText(svg, "text", "Bulan", "yText");
 
   /**
+   * LEGEND
+   */
+  const minTemp = min(monthlyVariance, (d) => d.temperature);
+  const maxTemp = max(monthlyVariance, (d) => d.temperature);
+
+  const legendWidth = 400;
+  const legendHeight = 300 / color.length;
+
+  const legendThreshold = thresholdScale(minTemp, maxTemp, color);
+  const legendXScale = linearScale(minTemp, maxTemp, 0, legendWidth);
+
+  /**
+   * LEGEND SVG
+   */
+  const legendContainer = svg.append("g").attr("id", "legend");
+
+  const legendData = legendThreshold.range().map((color) => {
+    const d = legendThreshold.invertExtent(color);
+    if (d[0] === null) {
+      d[0] = legendXScale.domain()[0];
+    }
+    if (d[1] === null) {
+      d[1] = legendXScale.domain()[1];
+    }
+    return d;
+  });
+
+  const legend = legendContainer
+    .selectAll("#legend")
+    .data(legendData)
+    .enter()
+    .append("rect")
+    .style("fill", (d) => legendThreshold(d[0]))
+    .attr("x", (d) => legendXScale(d[0]))
+    .attr("y", 0)
+    .attr("width", (d, i) => {
+      const fixedWidth = legendXScale(d[1]) - legendXScale(d[0]);
+      return d[0] && d[1] ? fixedWidth : legendXScale(null);
+    })
+    .attr("height", legendHeight);
+
+  const legendXAxis = createAxis("x", legendXScale, ".1f");
+
+  // legend
+  //   .append("rect")
+  //   .attr("x", svgWidth + 30)
+  //   .attr("width", 18)
+  //   .attr("height", 18)
+  //   .attr("class", "legend-rect")
+  //   .style("fill", color);
+
+  // legend
+  //   .append("text")
+  //   .attr("x", svgWidth + 25)
+  //   .attr("y", 9)
+  //   .attr("dy", ".35em")
+  //   .attr("class", "legend-label")
+  //   .style("text-anchor", "end")
+  //   .text((d) =>
+  //     d ? "Pesepeda dengan tuduhan doping" : "Tidak ada tuduhan doping"
+  //   );
+
+  /**
    * CIRCLE
    */
   // const dot = addCircle(svg, dataset, ".dot");
@@ -123,41 +186,6 @@ async function main() {
   //   .on("mouseout", (e, d) => {
   //     tooltip.style("opacity", 0);
   //   });
-
-  /**
-   * LEGEND
-   */
-  // const legendContainer = svg.append("g").attr("id", "legend");
-
-  // const legend = legendContainer
-  //   .selectAll("#legend")
-  //   .data(color.domain())
-  //   .enter()
-  //   .append("g")
-  //   .attr("class", "legend-label")
-  //   .attr(
-  //     "transform",
-  //     (_, i) => "translate(0," + (svgHeight / 2 - i * 30) + ")"
-  //   );
-
-  // legend
-  //   .append("rect")
-  //   .attr("x", svgWidth + 30)
-  //   .attr("width", 18)
-  //   .attr("height", 18)
-  //   .attr("class", "legend-rect")
-  //   .style("fill", color);
-
-  // legend
-  //   .append("text")
-  //   .attr("x", svgWidth + 25)
-  //   .attr("y", 9)
-  //   .attr("dy", ".35em")
-  //   .attr("class", "legend-label")
-  //   .style("text-anchor", "end")
-  //   .text((d) =>
-  //     d ? "Pesepeda dengan tuduhan doping" : "Tidak ada tuduhan doping"
-  //   );
 }
 
 try {

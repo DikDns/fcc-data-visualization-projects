5377cace334cc2c5939c75998c56bc654a587e0e
DikDns <dikdnssocial@gmail.com>
2023-05-24 08:49:03 +0700

    add: getSekolah and getProvinsi

--- src/modules/data.js
+++ src/modules/data.js
@@ -1,20 +1,20 @@
 import { getFetch } from "./fetch.js";
 import { round } from "./utils.js";
 
-const globalTemperatureUrl =
-  "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json";
+const SEKOLAH =
+  "https://raw.githubusercontent.com/DikDns/personal-project-data/main/fcc-choropleth-map/sekolah.json";
+const PROVINSI =
+  "https://raw.githubusercontent.com/DikDns/personal-project-data/main/fcc-choropleth-map/provinces-simplified-topo.json";
 
-export async function getGlobalTemperature() {
-  const data = await getFetch(globalTemperatureUrl);
-  return {
-    ...data,
-    // map Month to Zero-Indexes & calc temperature
-    monthlyVariance: data.monthlyVariance.map((d) => ({
-      ...d,
-      month: --d.month,
-      // Round to tenth decimal places (a.bcde => a.x)
-      temperature: round(data.baseTemperature + d.variance, 10),
-      variance: round(d.variance, 10),
-    })),
-  };
+export async function getSekolah() {
+  const data = await getFetch(SEKOLAH);
+  return data.map((d) => ({
+    ...d,
+    persentaseKesenjangan: round(d.jumlahMuridSMA / d.jumlahGuruSMA - 1, 100),
+  }));
+}
+
+export async function getProvinsi() {
+  const data = await getFetch(PROVINSI);
+  return data;
 }
